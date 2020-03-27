f697b8b1b681f56a0ef01ed81ba91d5876ea8912
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2020-03-28 00:48:47 +0530

    added basic auth for the client

--- src/client.py
+++ src/client.py
@@ -31,10 +31,10 @@ class IGCMD(Cmd):
             print(f'***Unknown Command : {inp}')
     
     def do_register(self, inp:str):
-        '''    Register to the IntraGIT service. \n    Usage: register <username> <password>'''
-        # parsed_inp = inp.split()
-        if inp is not None:
-            username = inp 
+        '''    Register to the IntraGIT service. \n    Usage: register <username>'''
+        if inp is not None:    
+            parsed_inp = inp.split()
+            username = parsed_inp[0]
             password = getpass.getpass()
             res = requests.post('https://localhost:5683/register', json={'username':username, 'password':password }, verify='ca-public-key.pem')
             if 'OK' in res.text:
@@ -44,6 +44,46 @@ class IGCMD(Cmd):
         else:
             print('***Too Less Arguments: register')
     
+    def do_login(self, inp:str):
+        '''    Login to the IntraGIT service. \n    Usage: login <username>'''
+        parsed_prompt = IGCMD.prompt.split()
+        if inp is not None and len(parsed_prompt) == 1:
+            username = inp
+            password = getpass.getpass()
+            res = requests.post('https://localhost:5683/login', json={'username':username, 'password':password }, verify='ca-public-key.pem')
+            if 'OK' in res.text:
+                print(f'Successfully logged in as {username}')
+                IGCMD.prompt = f'IGCMD {username}> '
+            elif 'Already' in res.text:
+                print(f'Already Logged in as {username}')
+                IGCMD.prompt = f'IGCMD {username}> '
+            else:
+                IGCMD.prompt = f'IGCMD> '
+                print(f'ERROR: {res.text}')
+        elif len(parsed_prompt) > 1:
+            print(f'***Already logged in as {parsed_prompt[1]}! Log Out First!')
+        else:
+            print('***Too Less Arguments: login')
+    
+    def do_logout(self, inp:str):
+        '''    Logout to the IntraGIT service. \n    Usage: logout <username>'''
+        parsed_prompt = IGCMD.prompt.split()
+        if inp is not None and len(parsed_prompt) > 1:
+            if inp+'>' == parsed_prompt[1]:
+                res = requests.post('https://localhost:5683/logout', json={'username':inp}, verify='ca-public-key.pem')
+                if 'OK' in res.text:
+                    print(f'Successfully logged out!')
+                    IGCMD.prompt = f'IGCMD> '
+                else:
+                    IGCMD.prompt = f'IGCMD> '
+                    print(f'ERROR : {res.text}')
+            else:
+                print(f'***Logout command issued for wrong user {inp}')
+                print(f'***Current User {parsed_prompt[1]}')
+        elif len(parsed_prompt) == 1:
+            print(f'***Invalid Command! No User Logged In')
+        else:
+            print(f'***Too Less Arguments')
     do_EOF = do_exit
 
 IGCMD().cmdloop()
--- src/gateway.py
+++ src/gateway.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from localdb import IGDB
 import hashlib
 import os
+from datetime import datetime
 
 SECRET_MESSAGE = 'very secret'
 app = Flask(__name__)
@@ -25,13 +26,75 @@ def resgister_client():
         hashed_username.update(username.encode('utf-8'))
         hashusr = hashed_username.hexdigest()
 
-        igdb = IGDB(os.path.join('dbs','users.json'))
+        igdb_users = IGDB(os.path.join('dbs','users.json'))
 
-        if not igdb.getd(hashusr):
+        if igdb_users.getd(hashusr) is None:
             data = {'username': username, 'password': password}
-            if igdb.setd(hashusr, data):
+            if igdb_users.setd(hashusr, data):
                 return 'OK'
             else: 
                 return ' DB Error'
         else:
             return ' User Already Registered'
+
+@app.route('/login', methods=['POST'])
+def login_client():
+    if request.method == 'POST':
+        username = request.json['username']
+        password = request.json['password']
+
+        hashed_username = hashlib.sha256()
+        hashed_username.update(username.encode('utf-8'))
+        hashusr = hashed_username.hexdigest()
+
+        igdb_users = IGDB(os.path.join('dbs', 'users.json'))
+        igdb_logclient = IGDB(os.path.join('dbs', 'logclient.json'))
+
+        data:dict = igdb_users.getd(hashusr) 
+        if data is not None:
+            #the user is registered
+            if password == data.get('password'):
+                #check password for login
+                luser:str = igdb_logclient.getd(hashusr)
+                if luser is None:
+                    #if user logs in for the first time
+                    dt = datetime.now()
+                    if igdb_logclient.setd(hashusr, dt.strftime(datetime.isoformat(dt))):
+                        return 'OK'
+                    else:
+                        return ' DB Error'
+                else:
+                    #if user in already logged in
+                    dt = datetime.now()
+                    fmt = dt.isoformat()
+                    if igdb_logclient.setd(hashusr, dt.strftime(datetime.isoformat(dt))):
+                        return 'Already logged in'
+                    else:
+                        return ' DB Error'
+            else:
+                if igdb_logclient.deld(hashusr):
+                    return ' Wrong Password! Please Try Again'
+                else:
+                    return ' DB Error with Wrong Password'
+        else:
+            #the user is not registered
+            return ' User Not Registered'
+
+@app.route('/logout', methods=['POST'])
+def logout_client():
+    if request.method == 'POST':
+        username = request.json['username']
+
+        hashed_username = hashlib.sha256()
+        hashed_username.update(username.encode('utf-8'))
+        hashusr = hashed_username.hexdigest()
+
+        igdb_logclient = IGDB(os.path.join('dbs','logclient.json'))
+
+        if igdb_logclient.getd(hashusr) is not None:
+            if igdb_logclient.deld(hashusr):
+                return 'OK'
+            else:
+                return ' DB Error while logging out'
+        else:
+            return ' User not logged in!'
